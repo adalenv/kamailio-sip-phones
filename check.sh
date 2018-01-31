266a7b4336bc4ae77ceec6968d6d838e73f890a1
Adalen Vladi <adalenvladi@gmail.com>
2018-01-31 11:29:06 +0100

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,1 +1,1 @@
-sudo sshpass -p "server password"  ssh root@server "cd /usr/local/kamailio-4.4/sbin ; ./kamctl ul show | grep "AOR" "
+sudo sshpass -p "password"  ssh root@host "cd /usr/local/kamailio-4.4/sbin ; ./kamctl ul show | grep "AOR" "
